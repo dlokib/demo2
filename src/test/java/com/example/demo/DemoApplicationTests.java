f6f595f5710902121d62f5777acefda7214461d2
dlokib <dlokib@gmail.com>
2021-01-04 23:18:25 +0400

    update test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,11 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
   @LocalServerPort
   private int port;
@@ -18,11 +19,10 @@ class DemoApplicationTests {
   @Autowired
   private TestRestTemplate restTemplate;
 
-  private String url = "http://localhost:8080/api/booking/status/5";
-
   @Test
   void contextLoads() {
-    String result = "{\"zoneId\":1,\"vehicleId\":1,\"ts_from\":\"2021-01-04T13:34:05.255+00:00\",\"ts_till\":\"2021-01-04T13:35:05.255+00:00\",\"cost\":40,\"payment\":false}";
+    String url = "http://localhost:" + port + "/api/booking/status/5";
+    String result = "{\"parkingId\":1,\"vehicleId\":1,\"ts_from\":\"2021-01-04T13:34:05.255+00:00\",\"ts_till\":\"2021-01-04T13:35:05.255+00:00\",\"cost\":40,\"payment\":false}";
 
     assertThat(this.restTemplate.getForObject(url,
       String.class)).contains(result);
